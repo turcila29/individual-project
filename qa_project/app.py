88a4392f70b756a56fbf3417957f087332303645
Turc <turcilaandrei30@gmail.com>
2023-08-11 15:23:10 +0100

    file update

--- qa_project/app.py
+++ qa_project/app.py
@@ -1,4 +1,4 @@
-from qa_project import app
+from application import app
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port='5001')
--- qa_project/application/routes.py
+++ qa_project/application/routes.py
@@ -1,2 +1,11 @@
 from application import app, db
 from application.models import Customer
+from flask import render_template
+
+
+@app.route("/")
+def home():
+    return render_template('layout.html')
+
+# @app.route("/")
+    
