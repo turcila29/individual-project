1c150eae4f9d6257062b8aa02c7c7b0b99a0a153
Turc <turcilaandrei30@gmail.com>
2023-08-11 15:23:00 +0100

    file update

--- qa_project/application/models.py
+++ qa_project/application/models.py
@@ -1,5 +1,23 @@
 from application import db
 
-class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
+
+# class Customers(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     firstName = db.Column(db.String(50), nullable = False)
+#     lastName = db.Column(db.String(50), nullable = False)
+#     email = db.Column(db.String(30), unique = True, nullable = False)
+#     Address = db.Column(db.String(60), nullable = False)
+#     phoneNumber = db.Column(db.Integer, nullable = False)
+#     # postalCode = db.Column
+
+# class Products(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     Name = db.Column(db.String(60), unique = True)
+#     Description = db.Column(db.String(100), nullable = False)
+#     Delivery = db.Relationship('Delivery', backref='products')
+    
+
+# class Delivery(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     orders_id = db.Column('orders_id', db.Integer, db.ForeignKey('orders.id'))
+#     products_id = db.Column('products_id', db.Integer, db.ForeignKey('products.id'))
